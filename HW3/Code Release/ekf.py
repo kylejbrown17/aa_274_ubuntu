27bf82ebe8f18812bf99b47ee11e085ded39bec2
Kyle Brown <kylejbrown17@gmail.com>
2017-02-28 15:14:05 -0800

    Everything seems to be working

--- HW3/Code Release/ekf.py
+++ HW3/Code Release/ekf.py
@@ -19,8 +19,8 @@ class EKF(object):
     # OUTPUT: none (internal belief state (self.x, self.P) should be updated)
     def transition_update(self, u, dt):
         g, Gx, Gu = self.transition_model(u, dt)
-        
-        self.x = g
+
+        self.x = np.reshape(g,[len(self.x),])
         self.P = Gx.dot(self.P).dot(Gx.T) + dt*Gu.dot(self.Q).dot(Gu.T)
         
         
@@ -47,10 +47,11 @@ class EKF(object):
         if z is None:    # don't update if measurement is invalid (e.g., no line matches for line-based EKF localization)
             return
 
-        sigma = H.dot(P).dot(H.T) + R
+        sigma = H.dot(self.P).dot(H.T) + R
         K = self.P.dot(H.T).dot(np.linalg.inv(sigma))
-        self.x = self.x + K.dot(z)
-        self.P = self.P.dot - K.dot(sigma).dot(K.T)
+        self.x = self.x + np.reshape(K.dot(z),[len(self.x),])
+        self.P = self.P - K.dot(sigma).dot(K.T)
+        # pdb.set_trace()
         
 
     # Converts raw measurement into the relevant Gaussian form (e.g., a dimensionality reduction);
@@ -79,11 +80,10 @@ class Localization_EKF(EKF):
         V, om = u
         x, y, Th = self.x        
         #pdb.set_trace()
-        
         if np.abs(u[1]) <= 0.00001: # account for singularity at omega = 0
-            g = np.array([[x + V*cos(Th)*dt],
-                          [y + V*sin(Th)*dt],
-                          [Th + om*dt]])
+            g = np.array([x + V*cos(Th)*dt,
+                          y + V*sin(Th)*dt,
+                          Th + om*dt])
             Gx = np.array([[1, 0, -V*sin(Th)*dt],
                            [0, 1, V*cos(Th)*dt],
                            [0, 0, 1]])
@@ -92,9 +92,9 @@ class Localization_EKF(EKF):
                            [0, dt]])
             
         else:
-            g = np.array([[x + (V/om)*(sin(om*dt + Th) - sin(Th))],
-                         [y + (V/om)*(-cos(om*dt + Th) + cos(Th))],
-                         [Th + om*dt]])
+            g = np.array([x + (V/om)*(sin(om*dt + Th) - sin(Th)),
+                          y + (V/om)*(-cos(om*dt + Th) + cos(Th)),
+                          Th + om*dt])
             # Gx
             dXdX = 1
             dXdY = 0
@@ -113,14 +113,15 @@ class Localization_EKF(EKF):
             dYdom = (V/om**2)*(om*np.sin(om*dt+Th)*dt + np.cos(om*dt+Th) - np.cos(Th))
             dThdV = 0
             dThdom = dt
-        
+                
             Gx = np.array([[dXdX, dXdY, dXdTh],
                            [dYdX, dYdY, dYdTh],
                            [dThdX, dThdY, dThdTh]])
             Gu = np.array([[dXdV, dXdom],
                            [dYdV, dYdom],
                            [dThdV, dThdom]])
-        pdb.set_trace()
+
+        
         return g, Gx, Gu
 
     # Given a single map line m in the world frame, outputs the line parameters in the scanner frame so it can
@@ -133,26 +134,30 @@ class Localization_EKF(EKF):
     def map_line_to_predicted_measurement(self, m):
         alpha, r = m
 
-        #### TODO ####
-        # compute h, Hx
-        ##############
         x, y, Th = self.x
-        A = alpha - Th - self.tf_base_to_camera
-        R = r -np.sqrt((x*np.cos(A))**2 + (y*sin(A))**2)
+        xC, yC, ThC = self.tf_base_to_camera
+        x = x + xC*np.cos(Th) - yC*np.sin(Th)
+        y = y + xC*np.sin(Th) + yC*np.cos(Th)
+        
+        A = alpha - Th - ThC
+        R = r - (x*np.cos(alpha) + y*np.sin(alpha))
 
         dAdx = 0
         dAdy = 0
         dAdTh = -1
-        dRdx = -( 1/(2*np.sqrt((x*np.cos(A))**2 + (y*np.sin(A))**2)) ) * 2*x*np.cos(A)**2
-        dRdy = -( 1/(2*np.sqrt((x*np.cos(A))**2 + (y*np.sin(A))**2)) ) * 2*y*np.sin(A)**2
-        dRdTh = -((2*x*np.cos(A) + 2*y*sin(A))/(2*np.sqrt((x*np.cos(A))**2 + (y*np.sin(A))**2))) * (-np.sin(A) + np.cos(A))
         
+        dRdx = -np.cos(alpha)
+        dRdy = -np.sin(alpha)
+        dRdTh = xC*np.cos(Th) - yC*np.sin(Th)
+
         h = np.array([[A],
                       [R]])
-
-        Hx = np.array([[dAdx, dAdy, dAdTh],
-                       [dRdx, dRdy, dRdTh]])
-
+        try:
+            Hx = np.array([[dAdx, dAdy, dAdTh],
+                           [dRdx, dRdy, dRdTh]])
+        except ValueError:
+            pdb.set_trace()
+            
         flipped, h = normalize_line_parameters(h)
         if flipped:
             Hx[1,:] = -Hx[1,:]
@@ -174,34 +179,31 @@ class Localization_EKF(EKF):
         # compute v_list, R_list, H_list
         ##############
 
-#        self.map_lines = map_lines                    # 2xJ matrix containing (alpha, r) for each of J map lines
-#        self.tf_base_to_camera = tf_base_to_camera    # (x, y, theta) transform from the robot base to the camera frame
-#        self.g = g                                    # validation gate
-
         d_list = []
         v_listA = []
         R_listA = []
         H_listA = []
 
         for i in range(len(rawR)):
+            d_list.append(None)
             v_listA.append(None)
             R_listA.append(None)
             H_listA.append(None)
             d_min = np.Inf
-            #v_min = None
-            #S_min = None
-            #R_min = None
-#        self.map_lines = map_lines                    # 2xJ matrix containing (alpha, r) for each of J map lines
             for j in range(self.map_lines.shape[1]):
-                m = (self.map_lines[:,j][0],self.map_lines[:,j][1])
+                m = self.map_lines[:,j]
                 h, Hx = self.map_line_to_predicted_measurement(m)
                 R = rawR[i]
-                z = rawZ[i,:]
-
+                z = np.array([rawZ[:,i]]).T
+                try:
+                    assert(z.shape[0] == 2)
+                except AssertionError:
+                    print z.shape
+                    
                 v = z - h
                 S = Hx.dot(self.P).dot(Hx.T) + R
 
-                d = (v.T).dot(S).dot(v)
+                d = (v.T).dot(np.linalg.inv(S)).dot(v)
                 if d[0][0] < d_min:
                     d_min = d[0][0]
                     d_list[i] = d_min
@@ -209,9 +211,9 @@ class Localization_EKF(EKF):
                     R_listA[i] = R
                     H_listA[i] = Hx
                     
-            v_list = [v_listA[i] for i in range(len(v_listA)) if v_listA[i] < self.g]
-            R_list = [R_listA[i] for i in range(len(R_listA)) if v_listA[i] < self.g]
-            H_list = [H_listA[i] for i in range(len(H_listA)) if v_listA[i] < self.g]
+            v_list = [v_listA[i] for i in range(len(v_listA)) if d_list[i] < self.g]
+            R_list = [R_listA[i] for i in range(len(R_listA)) if d_list[i] < self.g]
+            H_list = [H_listA[i] for i in range(len(H_listA)) if d_list[i] < self.g]
             
         return v_list, R_list, H_list
 
@@ -223,11 +225,11 @@ class Localization_EKF(EKF):
             print "Scanner sees", rawZ.shape[1], "line(s) but can't associate them with any map entries"
             return None, None, None
 
-        z = np.array([v_list]).T
+        z = np.vstack(v_list)
         
         R = np.zeros([2*len(R_list), 2*len(R_list)])
         for i in range(len(R_list)):
-            arr[2*i:2+2*i,2*i:2+2*i] = R_list[i]        
+            R[2*i:2+2*i,2*i:2+2*i] = R_list[i]        
 
         H = np.vstack(H_list)
         return z, R, H
@@ -297,7 +299,7 @@ class SLAM_EKF(EKF):
             Hx[1, 3+2*j+1] = FILLMEIN
 
         ##############
-
+        
         flipped, h = normalize_line_parameters(h)
         if flipped:
             Hx[1,:] = -Hx[1,:]
